d517cf74d5072040f6dc51bcd931ee752c8e8cc9
tonibauza <tonibauza@alumnat.iesdamiahuguet.net>
2024-01-16 13:01:32 +0100

    Commit boton alerta de palabra menor o mayor a la secreta hecho

---
+++ funcion.js
@@ -0,0 +1,13 @@
+const secreta='Papel'
+
+function BotonEscribir(){
+    let word= document.getElementById('escribirpalabra').value;
+
+    if (word>5/word<5){
+        window.alert('Una palabra de 5 letras debe ser')
+        let word= document.getElementById('escribirpalabra').value=''
+
+    }
+}
+//primero buscar que me pinte la palabra que escribo
+//poner alarma si palabra mayor o menor de 5 letras
