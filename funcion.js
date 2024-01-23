e46513b234c9a7ea8f0fb6d229c1c851dbe74baa
tonibauza <tonibauza@alumnat.iesdamiahuguet.net>
2024-01-23 12:34:59 +0100

    Commit palabra pinta en un slot

--- funcion.js
+++ funcion.js
@@ -1,13 +1,42 @@
-const secreta='Papel'
+const secreta='Panel'
 
 function BotonEscribir(){
     let word= document.getElementById('escribirpalabra').value;
 
-    if (word>5/word<5){
+    if (word.length!=5){
         window.alert('Una palabra de 5 letras debe ser')
-        let word= document.getElementById('escribirpalabra').value=''
+        document.getElementById('escribirpalabra').value=''
+
+    }else{
+        // NuevaEntrada='<div>' + value + '</div>'
+        for (let i=0;i<5;i++){
+            if (word.charAt(i)){
+                document.getElementById('letra0').innerText=word
+
+
+            }
+
+        }
 
     }
+//el For sirve para añadir los slots
+
+
+
 }
-//primero buscar que me pinte la palabra que escribo
-//poner alarma si palabra mayor o menor de 5 letras
+//TODO primero buscar que me pinte la palabra que escribo
+
+NuevaEntrada=''
+// function EscribirSlots(){
+//     NuevaEntrada= <div className="resposta">
+//         <div className="slot"></div>
+//         <div className="slot"></div>
+//         <div className="slot"></div>
+//         <div className="slot"></div>
+//         <div className="slot"></div>
+//     </div>
+//
+//
+// }
+
+//TODO DESPUES, hacer que pinte el cuadrado para añadir una palabra
