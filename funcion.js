cb820e46506e9d962d7db1fa6be1e5791eab0e45
ToniBau-za <tonibauza@alumnat.iesdamiahuguet.net>
2024-03-14 18:41:18 +0100

    Commit colores diferentes slots y varios hechos, pero no muestra el correcto

--- funcion.js
+++ funcion.js
@@ -11,14 +11,31 @@ function BotonEscribir(){
         let espacio="<div class='resposta'>"
         for (let i=0;i<word.length;i++){
             espacio += "<div class='slot'>"
-            espacio+= word.charAt(i)
+            espacio+= word.toUpperCase().charAt(i)
             espacio+="</div>"
 
         }
         espacio+= '</div>'
-        document.getElementById('respuesta').innerHTML= espacio + '<br>'
+        document.getElementById('respuesta').innerHTML+= espacio + '<br>'
 
+    }else{
+        let texto= "<div class='resposta'>"
+        for (let i=0;i<word.length;i++) {
+            if (word.toUpperCase().charAt(i).includes(secreta.charAt(i)) == true) {
+                texto += "<div class='slot yellow'>"
+
+            }else if (word.toUpperCase().charAt(i)==secreta.charAt(i)){
+                texto += "<div class='slot green'>"
+
+            }else{
+                texto += "<div class='slot'>"
+            }
+            texto += word.toUpperCase().charAt(i)
+            texto += '</div>'
+        }
+        document.getElementById('respuesta').innerHTML += texto + '<br>'
     }
+
 
 
 
