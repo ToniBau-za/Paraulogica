abee8d632aa8117984a74e4d4a0fa467531e5918
ToniBau-za <tonibauza@alumnat.iesdamiahuguet.net>
2024-03-14 18:49:58 +0100

    Commit comentarios puestos y color verde pinta pero solo si misma posicion y letra es igual

--- funcion.js
+++ funcion.js
@@ -1,17 +1,19 @@
+//La palabra secreta
 const secreta='PANEL'
 
 function BotonEscribir(){
     let word= document.getElementById('escribirpalabra').value;
 
+    //controla si alguna palabra supera el limite de 5 letras
     if (word.length!=5){
         window.alert('Una palabra de 5 letras debe ser')
         document.getElementById('escribirpalabra').value=''
 
-    }else if (word.toUpperCase()!=secreta){
+    }else if (word.toUpperCase()==secreta){
         let espacio="<div class='resposta'>"
-        for (let i=0;i<word.length;i++){
-            espacio += "<div class='slot'>"
-            espacio+= word.toUpperCase().charAt(i)
+        for (let i=0;i<secreta.length;i++){
+            espacio += "<div class='slot green'>"
+            espacio+= secreta.toUpperCase().charAt(i)
             espacio+="</div>"
 
         }
@@ -21,11 +23,12 @@ function BotonEscribir(){
     }else{
         let texto= "<div class='resposta'>"
         for (let i=0;i<word.length;i++) {
-            if (word.toUpperCase().charAt(i).includes(secreta.charAt(i)) == true) {
-                texto += "<div class='slot yellow'>"
-
-            }else if (word.toUpperCase().charAt(i)==secreta.charAt(i)){
+            //detecta si alguna letra es la misma y que esta en la posicion adequada de la palabra secreta
+            if (word.toUpperCase().charAt(i)==secreta.charAt(i)){
                 texto += "<div class='slot green'>"
+
+            }else if (word.toUpperCase().charAt(i).includes(secreta.charAt(i)) == true) {
+                texto += "<div class='slot yellow'>"
 
             }else{
                 texto += "<div class='slot'>"
